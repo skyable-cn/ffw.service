8fa24d52ae6afb9b48080440dcd4b37ec34abb01
mahonggang <1584157651@qq.com>
2019-04-14 00:55:31 +0800

    222

---
+++ src/main/java/com/ffw/service/service/impl/LotteryrecordServiceImpl.java
@@ -0,0 +1,57 @@
+package com.ffw.service.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.ffw.api.model.Page;
+import com.ffw.api.model.PageData;
+import com.ffw.service.dao.DaoSupport;
+import com.ffw.service.service.ILotteryrecordService;
+
+@Service
+public class LotteryrecordServiceImpl implements ILotteryrecordService {
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+
+	@Override
+	public List<PageData> listPage(Page page) throws Exception {
+		return (List<PageData>) dao.findForList("LotteryrecordMapper.listPage",
+				page);
+	}
+
+	@Override
+	public List<PageData> listAll(PageData pd) throws Exception {
+		return (List<PageData>) dao.findForList("LotteryrecordMapper.listAll",
+				pd);
+	}
+
+	@Override
+	public void save(PageData pd) throws Exception {
+		dao.save("LotteryrecordMapper.save", pd);
+	}
+
+	@Override
+	public void delete(PageData pd) throws Exception {
+		dao.delete("LotteryrecordMapper.delete", pd);
+	}
+
+	@Override
+	public void edit(PageData pd) throws Exception {
+		dao.update("LotteryrecordMapper.edit", pd);
+	}
+
+	@Override
+	public PageData findById(PageData pd) throws Exception {
+		return (PageData) dao.findForObject("LotteryrecordMapper.findById", pd);
+	}
+
+	@Override
+	public PageData findBy(PageData pd) throws Exception {
+		return (PageData) dao.findForObject("LotteryrecordMapper.findBy", pd);
+	}
+
+}
