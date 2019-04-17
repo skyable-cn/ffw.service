1e803884c61e550b292501d194c0795faca01d22
mahonggang <1584157651@qq.com>
2019-04-18 00:34:15 +0800

    ss

---
+++ src/main/java/com/ffw/service/controller/SupermaninfoController.java
@@ -0,0 +1,103 @@
+package com.ffw.service.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ffw.api.model.Page;
+import com.ffw.api.model.PageData;
+import com.ffw.service.service.ISupermaninfoService;
+
+@RestController
+@RequestMapping("supermaninfo")
+public class SupermaninfoController extends BaseController {
+
+	@Autowired
+	ISupermaninfoService supermaninfoService;
+
+	@RequestMapping(value = "listAll", method = RequestMethod.POST)
+	public List<PageData> listAll(@RequestBody PageData pd) {
+		List<PageData> data = null;
+		try {
+			data = supermaninfoService.listAll(pd);
+		} catch (Exception e) {
+			data = new ArrayList<PageData>();
+			e.printStackTrace();
+		}
+		return data;
+	}
+
+	@RequestMapping(value = "listPage", method = RequestMethod.POST)
+	public Page listPage(@RequestBody PageData pd) {
+		Page page = getPage(pd);
+		List<PageData> data = null;
+		try {
+			data = supermaninfoService.listPage(page);
+		} catch (Exception e) {
+			data = new ArrayList<PageData>();
+			e.printStackTrace();
+		}
+		page.setData(data);
+		return page;
+	}
+
+	@RequestMapping(value = "save", method = RequestMethod.POST)
+	public PageData save(@RequestBody PageData pd) {
+		try {
+			supermaninfoService.save(pd);
+		} catch (Exception e) {
+			pd = null;
+			e.printStackTrace();
+		}
+		return pd;
+	}
+
+	@RequestMapping(value = "edit", method = RequestMethod.POST)
+	public PageData edit(@RequestBody PageData pd) {
+		try {
+			supermaninfoService.edit(pd);
+		} catch (Exception e) {
+			pd = null;
+			e.printStackTrace();
+		}
+		return pd;
+	}
+
+	@RequestMapping(value = "delete", method = RequestMethod.POST)
+	public PageData delete(@RequestBody PageData pd) {
+		try {
+			supermaninfoService.delete(pd);
+		} catch (Exception e) {
+			pd = null;
+			e.printStackTrace();
+		}
+		return pd;
+	}
+
+	@RequestMapping(value = "find", method = RequestMethod.POST)
+	public PageData find(@RequestBody PageData pd) {
+		try {
+			pd = supermaninfoService.findById(pd);
+		} catch (Exception e) {
+			pd = null;
+			e.printStackTrace();
+		}
+		return pd;
+	}
+
+	@RequestMapping(value = "findBy", method = RequestMethod.POST)
+	public PageData findBy(@RequestBody PageData pd) {
+		try {
+			pd = supermaninfoService.findBy(pd);
+		} catch (Exception e) {
+			pd = null;
+			e.printStackTrace();
+		}
+		return pd;
+	}
+}
---
+++ src/main/java/com/ffw/service/service/impl/SupermaninfoServiceImpl.java
@@ -0,0 +1,57 @@
+package com.ffw.service.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.ffw.api.model.Page;
+import com.ffw.api.model.PageData;
+import com.ffw.service.dao.DaoSupport;
+import com.ffw.service.service.ISupermaninfoService;
+
+@Service
+public class SupermaninfoServiceImpl implements ISupermaninfoService {
+
+	@Resource(name = "daoSupport")
+	private DaoSupport dao;
+
+	@Override
+	public List<PageData> listPage(Page page) throws Exception {
+		return (List<PageData>) dao.findForList("SupermaninfoMapper.listPage",
+				page);
+	}
+
+	@Override
+	public List<PageData> listAll(PageData pd) throws Exception {
+		return (List<PageData>) dao.findForList("SupermaninfoMapper.listAll",
+				pd);
+	}
+
+	@Override
+	public void save(PageData pd) throws Exception {
+		dao.save("SupermaninfoMapper.save", pd);
+	}
+
+	@Override
+	public void delete(PageData pd) throws Exception {
+		dao.delete("SupermaninfoMapper.delete", pd);
+	}
+
+	@Override
+	public void edit(PageData pd) throws Exception {
+		dao.update("SupermaninfoMapper.edit", pd);
+	}
+
+	@Override
+	public PageData findById(PageData pd) throws Exception {
+		return (PageData) dao.findForObject("SupermaninfoMapper.findById", pd);
+	}
+
+	@Override
+	public PageData findBy(PageData pd) throws Exception {
+		return (PageData) dao.findForObject("SupermaninfoMapper.findBy", pd);
+	}
+
+}
